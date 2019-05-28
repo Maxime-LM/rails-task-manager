97077fceca1bd14cdb6c96a532815f7f7c6345ea
maxime le menaheze <maxime.le-menaheze@laposte.net>
2019-05-28 15:08:01 +0200

    new task btn

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,28 @@
 class TasksController < ApplicationController
+
   def index
-    @task = Task.all
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    task = Task.new(task_params)
+    # 2. Sauver l'objet en DB
+    task.save
+    # 3. Redirige vers la liste des tasks
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'tasks', to: 'tasks#create'
   get 'tasks', to: 'tasks#index'
+
+  get 'task/new', to: 'tasks#new', as: :new_task
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
 end
